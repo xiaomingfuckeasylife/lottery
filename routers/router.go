da11c61a1f77b276417aa6851b890e0bd6a974aa
xiaoming <1406629722@qq.com>
2018-07-10 17:58:45 +0800

    change api prefix

--- routers/router.go
+++ routers/router.go
@@ -14,7 +14,7 @@ import (
 )
 
 func init() {
-	ns := beego.NewNamespace("/api/v1",
+	ns := beego.NewNamespace("/lottery/v1",
 		beego.NSNamespace("/object",
 			beego.NSInclude(
 				&controllers.ObjectController{},
