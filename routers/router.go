17f2d79a78912b3aafa35386f45c15eb403fd7d9
xiaoming <1406629722@qq.com>
2018-07-10 18:35:51 +0800

    change router

--- routers/router.go
+++ routers/router.go
@@ -14,7 +14,7 @@ import (
 )
 
 func init() {
-	ns := beego.NewNamespace("/lottery/v1",
+	ns := beego.NewNamespace("/lottery/",
 		beego.NSNamespace("/object",
 			beego.NSInclude(
 				&controllers.ObjectController{},
