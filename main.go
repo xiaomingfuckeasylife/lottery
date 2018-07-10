10601dde727b6868b7981f1a0568b7933e501fce
xiaoming <1406629722@qq.com>
2018-07-10 18:47:18 +0800

    remove wechat validation

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
-		beego.SetStaticPath("/lottery/wechat", "wechat")
+		//beego.SetStaticPath("/lottery/wechat", "wechat")
 	}
 	defer db.Dia.Close()
 	beego.Run()
